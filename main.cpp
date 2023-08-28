daa3033388e28329283ba12fc8a334def30d8608
Benji <67171031+liangbinjie@users.noreply.github.com>
2023-08-28 01:27:14 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
-
 #include "pais/listaPais.h"
-#include "ciudad/listaCiudad.h"
-
 #include <iostream>
 using namespace std;
 
 int main() {
     ListaPais L1;
+    L1.cargarPaises();
     L1.insertar(12,"CR");
     L1.insertar(13,"EEUU");
     L1.insertar(14,"PNA");
