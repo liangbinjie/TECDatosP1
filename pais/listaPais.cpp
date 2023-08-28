94593e1137eb615483b5ab85be1846faa4cb77bd
Benji <67171031+liangbinjie@users.noreply.github.com>
2023-08-27 21:01:16 -0600

    Update listaPais.cpp

--- pais/listaPais.cpp
+++ pais/listaPais.cpp
@@ -46,11 +46,31 @@ void ListaPais::mostrar() {
     }
 }
 
+void ListaPais::borrarInicio() {
+    if (listaVacia()) {
+        cout << "No hay paises en la lista" << endl;
+    } else {
+        if (primero->siguiente == NULL) {
+            pNodoPais temp = primero;
+            primero = NULL;
+            delete temp;
+        } else {
+            pNodoPais aux = primero;
+            primero = primero->siguiente;
+            primero->anterior = NULL;
+            delete aux;
+        }
+    }
+}
+
 void ListaPais::eliminar(int codPais) {
     if (listaVacia()) {
         cout << "No se puede eliminar, lista vacia" << endl;
     } else {
         pNodoPais aux = primero;
+        if (aux->codigoPais == codPais) {
+            borrarInicio();
+        }
         cout << aux->codigoPais;
         while (aux->codigoPais != codPais) {
             aux = aux->siguiente;
@@ -60,4 +80,23 @@ void ListaPais::eliminar(int codPais) {
         aux->siguiente->anterior = aux->anterior;
         delete temp;
     }
+}
+
+bool ListaPais::existePais(int codPais) {
+    NodoPais *aux;
+    if (primero== NULL)
+        cout << "No hay elementos";  
+    else {
+        aux = primero;
+        while(aux) {
+            if (aux->codigoPais == codPais) {
+                cout << "El pais si existe" << endl;
+                return true;
+            }
+            aux = aux->siguiente;
+        }
+        
+    }
+    cout << "El pais no existe";
+    return false;
 }
