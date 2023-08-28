9e033b796250a4ba5bab6d045756c7d77d762b4f
Benji <67171031+liangbinjie@users.noreply.github.com>
2023-08-28 13:25:36 -0600

    Se agrego nodoRest

---
+++ restaurant/nodoRest.h
@@ -0,0 +1,23 @@
+#ifndef NODOREST_H
+#define NODOREST_H
+
+#include <iostream>
+using namespace std;
+
+class NodoRest {
+    private:
+        int codigoPais;
+        int codCiudad;
+        int codRest;
+        string nombre;
+        NodoRest *siguiente;
+        NodoRest *anterior;
+        
+        friend class ListaRest;
+
+    public:
+        NodoRest(int pCodPais, int pCodCiudad, int pCodRest);
+        NodoRest(int pCodPais, int pCodCiudad, int pCodRest, NodoRest* sigNodo);
+}
+
+#endif
